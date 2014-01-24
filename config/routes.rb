ba5ce498f0d99e8fff72b65df6991cbbe1457885
matt-mcelwain <matt-mcelwain>
2014-01-24 19:34:38 +0000

    Tutorial finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 NkuRails::Application.routes.draw do
+  root 'posts#new'
+  get "posts/new"
+  resources :posts do
+    resources :comments
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
